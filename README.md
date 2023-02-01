a3da8c020218caa4c953417b6b904cb8e18a73fa
Pigeon Lover <117943627+Pigeonlover@users.noreply.github.com>
2023-02-01 22:28:53 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Utilities App
+
+App with various simple utilities that can be used.
+Currently includes:
+
+1) Current date
+2) Current time
---
+++ dateTime.js
@@ -0,0 +1,46 @@
+function currentDateTime() {
+    let dateTime = new Date();
+
+    let year = dateTime.getFullYear();
+    let month = dateTime.getMonth();
+    let day = dateTime.getDay();
+
+    let hour = dateTime.getHours();
+    let minutes = dateTime.getMinutes();
+    let seconds = dateTime.getSeconds();
+
+    let midday = "AM";
+
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (day < 10) {
+        day = "0" + day;
+    }
+
+    if (hour > 12) {
+        hour = hour - 12;
+        midday = "PM";
+    }
+
+    if (hour < 10) {
+        hour = "0" + hour;
+    } 
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    let date = `${year}-${month}-${day}`;
+    let time =  `${hour}:${minutes}:${seconds} ${midday}`;
+
+    document.getElementById("dateTimeContainer").innerText = `${date}\n${time}`;
+
+    let update = setTimeout(function() {currentDateTime()}, 1000);
+
+}
+
+
+currentDateTime();
