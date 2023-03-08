62d6c069c79d841f1abaa0b7be066681d1eba90a
Pigeonlover <bravefoot@outlook.com>
2023-03-08 21:59:07 +0000

    Added the stopwatch :D

--- stopwatch.js
+++ stopwatch.js
@@ -1,10 +1,61 @@
-let reset = document.getElementById('swReset');
-let start = document.getElementById('swStart');
-let stop = document.getElementById('swStop');
+// Getting the elements from the HTML
+const resetButton = document.getElementById('swReset');
+const startButton = document.getElementById('swStart');
+const stopButton = document.getElementById('swStop');
 
-let hours = 00;
-let minutes = 00;
-let seconds = 00;
-let milSeconds = 00; 
+startButton.addEventListener('click', startCount);
+stopButton.addEventListener('click', stopCount);
+resetButton.addEventListener('click', resetCount);
 
-document.getElementById("swScreen").innerText = `${hours} : ${minutes} : ${seconds} : ${milSeconds}`;
+// Variables needed for the general running
+let startTime;
+let elapsedTime = 0;
+let timeInterval;
+
+// Conversion to hours, minutes, seconds, and milliseconds
+function timeConversion(time) {
+    let hourDiff = time / 3600000;
+    let hh = Math.floor(hourDiff);
+
+    let minsDiff = (hourDiff - hh) * 60;
+    let mm = Math.floor(minsDiff);
+
+    let secsDiff = (minsDiff - mm) * 60;
+    let ss = Math.floor(secsDiff);
+
+    let millsDiff = (secsDiff - ss) * 100;
+    let ms = Math.floor(millsDiff);
+
+    let formatHH = hh.toString().padStart(2, '0');
+    let formatMM = mm.toString().padStart(2, '0');
+    let formatSS = ss.toString().padStart(2, '0');
+    let formatMS = ms.toString().padStart(2, '0');
+
+    return `${formatHH}:${formatMM}:${formatSS}:${formatMS}`;
+}
+
+
+// Show count in the HTML
+function printCount(text) {
+    document.getElementById('swScreen').innerHTML = text;
+}
+
+
+// Start, stop, and reset functions
+function startCount() {
+    startTime = Date.now() - elapsedTime;
+    timeInterval = setInterval(function printTime(){
+        elapsedTime = Date.now() - startTime;
+        printCount(timeConversion(elapsedTime))
+    }, 10)
+}
+
+function stopCount() {
+    clearInterval(timeInterval);
+}
+
+function resetCount() {
+    clearInterval(timeInterval);
+    printCount('00:00:00:00');
+    elapsedTime = 0;
+}
